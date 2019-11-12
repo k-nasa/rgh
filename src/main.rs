8684745e8db34d385e9090ce5635b09e307585cc
k-nasa <htilcs1115@gmail.com>
2019-11-12 11:22:28 +0900

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -30,10 +30,49 @@ fn build_app() -> App<'static, 'static> {
         .version(crate_version!())
         .about(crate_description!())
         .setting(AppSettings::DeriveDisplayOrder)
+        .setting(AppSettings::ColoredHelp)
         .args(&[
             Arg::with_name("tag").help("tag").required(true),
             Arg::with_name("packages")
                 .help("upload packages dir or file")
                 .required(true),
         ])
+        .arg(
+            Arg::with_name("target_commitish")
+                .help("Specifies the commitish value that determines where the Git tag is created from. Can be any branch or commit SHA. Unused if the Git tag already exists. Default: the repository's default branch (usually master).")
+                .long("target-commitish")
+                .value_name("target-commitish"),
+        )
+        .arg(
+            Arg::with_name("token")
+            .help("Set Github API Token")
+            .long("token")
+            .short("t")
+            .value_name("token"),
+            )
+        .arg(
+            Arg::with_name("title")
+            .help("The title of the release")
+            .long("title")
+            .value_name("name"),
+            )
+        .arg(
+            Arg::with_name("body")
+            .help("Text describing the contents of the tag.")
+            .long("body")
+            .short("b")
+            .value_name("body"),
+            )
+        .arg(
+            Arg::with_name("draft")
+            .long("draft")
+            .value_name("draft")
+            .possible_values(&["true", "false"])
+            )
+        .arg(
+            Arg::with_name("prerelease")
+            .long("prerelease")
+            .value_name("prerelease")
+            .possible_values(&["true", "false"])
+            )
 }
